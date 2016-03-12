16dccf5be79de4293bc961db289fd91c2c7f93f0
rigaya <rigaya34589@live.jp>
2016-03-12 09:59:53 +0900

    ファイルヘッダー修正。

--- NVEnc/frm/frmNewFolderName.h
+++ NVEnc/frm/frmNewFolderName.h
@@ -3,7 +3,7 @@
 // -----------------------------------------------------------------------------------------
 // The MIT License
 //
-// Copyright (c) 1999-2016 rigaya
+// Copyright (c) 2014-2016 rigaya
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
