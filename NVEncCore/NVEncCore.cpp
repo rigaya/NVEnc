cd0b2101b3afd79a14cdc3ede736ab3edbb5505d
rigaya <rigaya34589@live.jp>
2017-08-27 18:11:25 +0900

    vpp-afs: 入力からインタレのフィールドオーダーの情報を取得できていない場合、ユーザーに指定してもらう必要があるので、エラー終了するように。

--- NVEncCore/NVEncCore.cpp
+++ NVEncCore/NVEncCore.cpp
@@ -2589,6 +2589,10 @@ NVENCSTATUS NVEncCore::InitFilters(const InEncodeVideoParam *inputParam) {
         }
         //afs
         if (inputParam->vpp.afs.enable) {
+            if ((inputParam->input.picstruct & (RGY_PICSTRUCT_TFF | RGY_PICSTRUCT_BFF)) == 0) {
+                PrintMes(RGY_LOG_ERROR, _T("Please set interlace field order (tff/bff) for vpp-afs.\n"));
+                return NV_ENC_ERR_INVALID_PARAM;
+            }
             unique_ptr<NVEncFilter> filter(new NVEncFilterAfs());
             shared_ptr<NVEncFilterParamAfs> param(new NVEncFilterParamAfs());
             param->afs = inputParam->vpp.afs;
