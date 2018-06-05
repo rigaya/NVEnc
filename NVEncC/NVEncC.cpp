ae41208fa0a43176223e22a73191876cc058f638
rigaya <rigaya34589@live.jp>
2018-06-05 22:16:00 +0900

    4.02以降、浮動小数点がうまく読めないという指摘があったので、ロケールの指定をするようにした変更( b2c33672 )を元に戻す。( #54 )

--- NVEncC/NVEncC.cpp
+++ NVEncC/NVEncC.cpp
@@ -925,8 +925,6 @@ int _tmain(int argc, TCHAR **argv) {
 #if defined(_WIN32) || defined(_WIN64)
     if (check_locale_is_ja()) {
         _tsetlocale(LC_ALL, _T("Japanese"));
-    } else {
-        _tsetlocale(LC_ALL, _T(""));
     }
 #endif //#if defined(_WIN32) || defined(_WIN64)
 
