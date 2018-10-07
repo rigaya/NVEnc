4aa1d20227545fa21abe11b6ed28f7d0d6e31397
rigaya <rigaya34589@live.jp>
2018-10-07 19:58:30 +0900

    nal_infoのポインタにconstを再付与。

--- NVEncCore/rgy_bitstream.h
+++ NVEncCore/rgy_bitstream.h
@@ -34,7 +34,7 @@
 #include <string>
 
 struct nal_info {
-    uint8_t *ptr;
+    const uint8_t *ptr;
     uint8_t type;
     uint32_t size;
 };
@@ -69,7 +69,7 @@ enum : uint8_t {
     NALU_HEVC_SUFFIX_SEI = 40,
 };
 
-static std::vector<nal_info> parse_nal_unit_h264(uint8_t *data, uint32_t size) {
+static std::vector<nal_info> parse_nal_unit_h264(const uint8_t *data, uint32_t size) {
     std::vector<nal_info> nal_list;
     nal_info nal_start = { nullptr, 0, 0 };
     const int i_fin = size - 3;
@@ -94,7 +94,7 @@ static std::vector<nal_info> parse_nal_unit_h264(uint8_t *data, uint32_t size) {
     return nal_list;
 }
 
-static std::vector<nal_info> parse_nal_unit_hevc(uint8_t *data, uint32_t size) {
+static std::vector<nal_info> parse_nal_unit_hevc(const uint8_t *data, uint32_t size) {
     std::vector<nal_info> nal_list;
     nal_info nal_start = { nullptr, 0, 0 };
     const int i_fin = size - 3;
