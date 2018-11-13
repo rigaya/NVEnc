b9465b77659edd0ca54c6af43e60ff4e8805017b
rigaya <rigaya34589@live.jp>
2018-11-14 07:15:03 +0900

    92307838 の修正が不十分だったのを修正。

--- NVEncCore/rgy_input_avs.cpp
+++ NVEncCore/rgy_input_avs.cpp
@@ -122,7 +122,7 @@ RGY_ERR RGYInputAvs::Init(const TCHAR *strFileName, VideoInfo *pInputInfo, const
         return RGY_ERR_INVALID_HANDLE;
     }
 
-    const auto interface_ver = (m_sAvisynth.is_420 && m_sAvisynth.is_422 && m_sAvisynth.is_444) ? AVISYNTH_INTERFACE_VERSION : 5;
+    const auto interface_ver = (m_sAvisynth.is_420 && m_sAvisynth.is_422 && m_sAvisynth.is_444) ? AVISYNTH_INTERFACE_VERSION : RGY_AVISYNTH_INTERFACE_25;
     if (nullptr == (m_sAVSenv = m_sAvisynth.create_script_environment(interface_ver))) {
         AddMessage(RGY_LOG_ERROR, _T("failed to init avisynth enviroment.\n"));
         return RGY_ERR_INVALID_HANDLE;
