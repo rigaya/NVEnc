05d74c3eaaf6372c6308d9c3a49301b58734e3d7
rigaya <rigaya34589@live.jp>
2018-11-17 15:36:10 +0900

    muxなしで出力すると、caption2assを使用しないときでもメッセージが出ていたのを修正。

--- NVEncCore/rgy_input_avcodec.cpp
+++ NVEncCore/rgy_input_avcodec.cpp
@@ -2041,13 +2041,15 @@ HANDLE RGYInputAvcodec::getThreadHandleInput() {
 
 //出力する動画の情報をセット
 void RGYInputAvcodec::setOutputVideoInfo(int w, int h, int sar_x, int sar_y, bool mux) {
-    if (mux) {
-        m_cap2ass.setOutputResolution(w, h, sar_x, sar_y);
-        m_cap2ass.printParam(RGY_LOG_DEBUG);
-    } else {
-        //muxしないなら処理する必要はない
-        AddMessage(RGY_LOG_WARN, _T("caption2ass will be disabled as muxer is disabled.\n"));
-        m_cap2ass.disable();
+    if (m_cap2ass.enabled()) {
+        if (mux) {
+            m_cap2ass.setOutputResolution(w, h, sar_x, sar_y);
+            m_cap2ass.printParam(RGY_LOG_DEBUG);
+        } else {
+            //muxしないなら処理する必要はない
+            AddMessage(RGY_LOG_WARN, _T("caption2ass will be disabled as muxer is disabled.\n"));
+            m_cap2ass.disable();
+        }
     }
 }
 
