23a0606e218551e77538eee0f33fc7c9620b55fb
rigaya <rigaya34589@live.jp>
2018-12-10 22:20:32 +0900

    widthが32で割り切れない場合に範囲外アクセスの例外で落ちる可能性があったのを修正。

--- NVEnc/encode/auo_video.cpp
+++ NVEnc/encode/auo_video.cpp
@@ -111,7 +111,14 @@ void get_csp_and_bitdepth(bool& use_highbit, RGY_CSP& csp, const CONF_GUIEX *con
 
 static int calc_input_frame_size(int width, int height, int color_format) {
     width = (color_format == CF_RGB) ? (width+3) & ~3 : (width+1) & ~1;
-    return width * height * COLORFORMATS[color_format].size;
+    //widthが割り切れない場合、多めにアクセスが発生するので、そのぶんを確保しておく
+    const DWORD pixel_size = COLORFORMATS[color_format].size;
+    const DWORD simd_check = get_availableSIMD();
+    const DWORD align_size = (simd_check & AUO_SIMD_SSE2) ? ((simd_check & AUO_SIMD_AVX2) ? 64 : 32) : 1;
+#define ALIGN_NEXT(i, align) (((i) + (align-1)) & (~(align-1))) //alignは2の累乗(1,2,4,8,16,32...)
+    const DWORD frame_size = ALIGN_NEXT(width * height * pixel_size + (ALIGN_NEXT(width, align_size / pixel_size) - width) * 2 * pixel_size, align_size);
+#undef ALIGN_NEXT
+    return frame_size;
 }
 
 BOOL setup_afsvideo(const OUTPUT_INFO *oip, const SYSTEM_DATA *sys_dat, CONF_GUIEX *conf, PRM_ENC *pe) {
