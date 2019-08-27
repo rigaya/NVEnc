346375c330c77793b56883f8f6bea699d5b1d2b8
rigaya <rigaya34589@live.jp>
2019-08-27 23:30:34 +0900

    不要な改行を削除。

--- NVEncCore/NVEncCore.cpp
+++ NVEncCore/NVEncCore.cpp
@@ -4348,7 +4348,6 @@ NVENCSTATUS NVEncCore::Encode() {
         }
     }
 #endif //#if ENABLE_AVSW_READER
-    PrintMes(RGY_LOG_INFO, _T("                                                                             \n"));
     //FlushEncoderはかならず行わないと、NvEncDestroyEncoderで異常終了する
     auto encstatus = nvStatus;
     if (nEncodeFrames > 0 || nvStatus == NV_ENC_SUCCESS) {
