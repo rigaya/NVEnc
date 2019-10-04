1d919ebab2d59e6454def3a77096761906bb2a86
rigaya <rigaya34589@live.jp>
2019-10-04 13:41:01 +0900

    バージョン番号を更新。 ( #159 )

--- NVEncCore/rgy_version.h
+++ NVEncCore/rgy_version.h
@@ -29,9 +29,9 @@
 #ifndef __RGY_CONFIG_H__
 #define __RGY_CONFIG_H__
 
-#define VER_FILEVERSION              0,4,49,0
-#define VER_STR_FILEVERSION          "4.49"
-#define VER_STR_FILEVERSION_TCHAR _T("4.49")
+#define VER_FILEVERSION              0,4,51,0
+#define VER_STR_FILEVERSION          "4.51"
+#define VER_STR_FILEVERSION_TCHAR _T("4.51")
 
 #ifdef _M_IX86
 #define BUILD_ARCH_STR _T("x86")
