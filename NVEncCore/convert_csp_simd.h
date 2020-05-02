be449abb370557d996c5b534721e3fa8b729b97f
rigaya <rigaya34589@live.jp>
2020-05-03 06:49:48 +0900

    memcpyをQSVEnc/VCEEncに合わせる。

--- NVEncCore/convert_csp_simd.h
+++ NVEncCore/convert_csp_simd.h
@@ -48,6 +48,7 @@ static void RGY_FORCEINLINE memcpy_sse(uint8_t *dst, const uint8_t *src, int siz
         return;
     }
     uint8_t *dst_fin = dst + size;
+    uint8_t *dst_aligned_fin = (uint8_t *)(((size_t)(dst_fin + 15) & ~15) - 64);
     __m128 x0, x1, x2, x3;
     const int start_align_diff = (int)((size_t)dst & 15);
     if (start_align_diff) {
@@ -55,10 +56,8 @@ static void RGY_FORCEINLINE memcpy_sse(uint8_t *dst, const uint8_t *src, int siz
         _mm_storeu_ps((float*)dst, x0);
         dst += 16 - start_align_diff;
         src += 16 - start_align_diff;
-        size -= (16 - start_align_diff);
     }
-    x0 = _mm_loadu_ps((float *)(src +  0));
-    for (; size > 64; size -= 64, src += 64, dst += 64) {
+    for ( ; dst < dst_aligned_fin; dst += 64, src += 64) {
         x0 = _mm_loadu_ps((float*)(src +  0));
         x1 = _mm_loadu_ps((float*)(src + 16));
         x2 = _mm_loadu_ps((float*)(src + 32));
