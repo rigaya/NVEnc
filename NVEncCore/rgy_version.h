a1aa9bafa06b24ccfa391c31df6f332d9ed84aa0
rigaya <rigaya34589@live.jp>
2020-06-12 20:05:52 +0900

    バージョン番号の上げ忘れを修正。

--- NVEncCore/rgy_version.h
+++ NVEncCore/rgy_version.h
@@ -29,9 +29,9 @@
 #ifndef __RGY_CONFIG_H__
 #define __RGY_CONFIG_H__
 
-#define VER_FILEVERSION              0,5,3,0
-#define VER_STR_FILEVERSION          "5.03"
-#define VER_STR_FILEVERSION_TCHAR _T("5.03")
+#define VER_FILEVERSION              0,5,4,0
+#define VER_STR_FILEVERSION          "5.04"
+#define VER_STR_FILEVERSION_TCHAR _T("5.04")
 
 #ifdef _M_IX86
 #define BUILD_ARCH_STR _T("x86")
