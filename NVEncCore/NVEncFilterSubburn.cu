0eb5a8f9a1b71d57b44bc29a6c6a9f984fa9b469
rigaya <rigaya34589@live.jp>
2021-01-09 20:04:48 +0900

    一部のassでinvalid configurationというエラーで異常終了するのを修正。 ( #293 )
    width, heightが0になるケースがあった。

--- NVEncCore/NVEncFilterSubburn.cu
+++ NVEncCore/NVEncFilterSubburn.cu
@@ -136,7 +136,7 @@ cudaError_t proc_frame(FrameInfo *pFrame,
     //焼きこみフレームの範囲内に収まるようチェック
     const int burnWidth  = std::min((pos_x & ~1) + pSubImg->width,  pFrame->width)  - (pos_x & ~1);
     const int burnHeight = std::min((pos_y & ~1) + pSubImg->height, pFrame->height) - (pos_y & ~1);
-    if (burnWidth < 0 || burnHeight < 0) {
+    if (burnWidth <= 0 || burnHeight <= 0) {
         return cudaSuccess;
     }
 
--- NVEncCore/rgy_version.h
+++ NVEncCore/rgy_version.h
@@ -29,9 +29,9 @@
 #ifndef __RGY_CONFIG_H__
 #define __RGY_CONFIG_H__
 
-#define VER_FILEVERSION              0,5,24,0
-#define VER_STR_FILEVERSION          "5.24"
-#define VER_STR_FILEVERSION_TCHAR _T("5.24")
+#define VER_FILEVERSION              0,5,25,0
+#define VER_STR_FILEVERSION          "5.25"
+#define VER_STR_FILEVERSION_TCHAR _T("5.25")
 
 #ifdef _M_IX86
 #define BUILD_ARCH_STR _T("x86")
