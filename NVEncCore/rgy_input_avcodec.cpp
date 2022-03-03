bb021a18a852fa5ca659236628fdf9a28389f067
rigaya <rigaya34589@live.jp>
2022-03-03 19:09:20 +0900

    inFormatの宣言方法を変更。

--- NVEncCore/rgy_input_avcodec.cpp
+++ NVEncCore/rgy_input_avcodec.cpp
@@ -1265,7 +1265,7 @@ RGY_ERR RGYInputAvcodec::initFormatCtx(const TCHAR *strFileName, const RGYInputA
         scan_all_pmts_set = true;
     }
     //入力フォーマットが指定されていれば、それを渡す
-    const AVInputFormat *inFormat = nullptr;
+    decltype(av_find_input_format(nullptr)) inFormat = nullptr;
     if (input_prm->pInputFormat) {
         if (nullptr == (inFormat = av_find_input_format(tchar_to_string(input_prm->pInputFormat).c_str()))) {
             AddMessage(RGY_LOG_ERROR, _T("Unknown Input format: %s.\n"), input_prm->pInputFormat);
@@ -1309,7 +1309,7 @@ RGY_ERR RGYInputAvcodec::initFormatCtx(const TCHAR *strFileName, const RGYInputA
         }
     }
     //ファイルのオープン
-    if ((ret = avformat_open_input(&(m_Demux.format.formatCtx), filename_char.c_str(), (RGYArgN<2U, decltype(avformat_open_input)>::type)inFormat, &m_Demux.format.formatOptions)) != 0) {
+    if ((ret = avformat_open_input(&(m_Demux.format.formatCtx), filename_char.c_str(), inFormat, &m_Demux.format.formatOptions)) != 0) {
         AddMessage(RGY_LOG_ERROR, _T("error opening file \"%s\": %s\n"), char_to_tstring(filename_char, CP_UTF8).c_str(), qsv_av_err2str(ret).c_str());
         return RGY_ERR_FILE_OPEN; // Couldn't open file
     }
