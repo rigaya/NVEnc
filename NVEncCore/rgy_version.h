c21de11d3aa490ee87b7ce1af58b78dd1a71cd9b
rigaya <rigaya34589@live.jp>
2022-07-29 21:50:28 +0900

    バージョン6.06。

--- NVEncCore/rgy_version.h
+++ NVEncCore/rgy_version.h
@@ -29,9 +29,9 @@
 #ifndef __RGY_CONFIG_H__
 #define __RGY_CONFIG_H__
 
-#define VER_FILEVERSION              0,6,6,0
-#define VER_STR_FILEVERSION          "6.06 beta1"
-#define VER_STR_FILEVERSION_TCHAR _T("6.06 beta1")
+#define VER_FILEVERSION              0,6,6,1
+#define VER_STR_FILEVERSION          "6.06"
+#define VER_STR_FILEVERSION_TCHAR _T("6.06")
 
 #ifdef _M_IX86
 #define BUILD_ARCH_STR _T("x86")
