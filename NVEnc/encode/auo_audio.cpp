d885322d229543649ffa444248322d004604a249
rigaya <rigaya34589@live.jp>
2022-08-13 10:19:13 +0900

    ヘッダの更新。

--- NVEnc/encode/auo_audio.cpp
+++ NVEnc/encode/auo_audio.cpp
@@ -3,7 +3,7 @@
 // -----------------------------------------------------------------------------------------
 // The MIT License
 //
-// Copyright (c) 2011-2022 rigaya
+// Copyright (c) 2010-2022 rigaya
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
