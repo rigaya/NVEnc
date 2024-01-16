3e855d7035701d49616dcfc4845e1caa6d663966
rigaya <rigaya34589@live.jp>
2024-01-16 19:30:29 +0900

    block_size=16への対応。

--- NVEncCore/NVEncFilterDenoiseDct.cu
+++ NVEncCore/NVEncFilterDenoiseDct.cu
@@ -151,10 +151,200 @@ __device__ void CUDAsubroutineInplaceIDCT8vector(T *Vect0) {
 }
 
 template<typename T, int Step>
-__device__ void CUDAsubroutineInplaceDCT16vector(T *Vect0) {}
+__device__ void  CUDAsubroutineInplaceDCT16vector(T *Vect00) {
+    T *Vect01 = Vect00 + Step;
+    T *Vect02 = Vect01 + Step;
+    T *Vect03 = Vect02 + Step;
+    T *Vect04 = Vect03 + Step;
+    T *Vect05 = Vect04 + Step;
+    T *Vect06 = Vect05 + Step;
+    T *Vect07 = Vect06 + Step;
+
+    T *Vect08 = Vect00 + (Step << 3);
+    T *Vect09 = Vect08 + Step;
+    T *Vect10 = Vect09 + Step;
+    T *Vect11 = Vect10 + Step;
+    T *Vect12 = Vect11 + Step;
+    T *Vect13 = Vect12 + Step;
+    T *Vect14 = Vect13 + Step;
+    T *Vect15 = Vect14 + Step;
+
+    const float x00 = (*Vect00) + (*Vect15);
+    const float x01 = (*Vect01) + (*Vect14);
+    const float x02 = (*Vect02) + (*Vect13);
+    const float x03 = (*Vect03) + (*Vect12);
+    const float x04 = (*Vect04) + (*Vect11);
+    const float x05 = (*Vect05) + (*Vect10);
+    const float x06 = (*Vect06) + (*Vect09);
+    const float x07 = (*Vect07) + (*Vect08);
+    const float x08 = (*Vect00) - (*Vect15);
+    const float x09 = (*Vect01) - (*Vect14);
+    const float x0a = (*Vect02) - (*Vect13);
+    const float x0b = (*Vect03) - (*Vect12);
+    const float x0c = (*Vect04) - (*Vect11);
+    const float x0d = (*Vect05) - (*Vect10);
+    const float x0e = (*Vect06) - (*Vect09);
+    const float x0f = (*Vect07) - (*Vect08);
+    const float x10 = x00 + x07;
+    const float x11 = x01 + x06;
+    const float x12 = x02 + x05;
+    const float x13 = x03 + x04;
+    const float x14 = x00 - x07;
+    const float x15 = x01 - x06;
+    const float x16 = x02 - x05;
+    const float x17 = x03 - x04;
+    const float x18 = x10 + x13;
+    const float x19 = x11 + x12;
+    const float x1a = x10 - x13;
+    const float x1b = x11 - x12;
+    const float x1c =   1.38703984532215f*x14 + 0.275899379282943f*x17;
+    const float x1d =   1.17587560241936f*x15 + 0.785694958387102f*x16;
+    const float x1e = -0.785694958387102f*x15 + 1.17587560241936f *x16;
+    const float x1f =  0.275899379282943f*x14 - 1.38703984532215f *x17;
+    const float x20 = 0.25f * (x1c - x1d);
+    const float x21 = 0.25f * (x1e - x1f);
+    const float x22 =  1.40740373752638f *x08 + 0.138617169199091f*x0f;
+    const float x23 =  1.35331800117435f *x09 + 0.410524527522357f*x0e;
+    const float x24 =  1.24722501298667f *x0a + 0.666655658477747f*x0d;
+    const float x25 =  1.09320186700176f *x0b + 0.897167586342636f*x0c;
+    const float x26 = -0.897167586342636f*x0b + 1.09320186700176f *x0c;
+    const float x27 =  0.666655658477747f*x0a - 1.24722501298667f *x0d;
+    const float x28 = -0.410524527522357f*x09 + 1.35331800117435f *x0e;
+    const float x29 =  0.138617169199091f*x08 - 1.40740373752638f *x0f;
+    const float x2a = x22 + x25;
+    const float x2b = x23 + x24;
+    const float x2c = x22 - x25;
+    const float x2d = x23 - x24;
+    const float x2e = 0.25f * (x2a - x2b);
+    const float x2f = 0.326640741219094f*x2c + 0.135299025036549f*x2d;
+    const float x30 = 0.135299025036549f*x2c - 0.326640741219094f*x2d;
+    const float x31 = x26 + x29;
+    const float x32 = x27 + x28;
+    const float x33 = x26 - x29;
+    const float x34 = x27 - x28;
+    const float x35 = 0.25f * (x31 - x32);
+    const float x36 = 0.326640741219094f*x33 + 0.135299025036549f*x34;
+    const float x37 = 0.135299025036549f*x33 - 0.326640741219094f*x34;
+    (*Vect00) = 0.25f * (x18 + x19);
+    (*Vect01) = 0.25f * (x2a + x2b);
+    (*Vect02) = 0.25f * (x1c + x1d);
+    (*Vect03) = 0.707106781186547f * (x2f - x37);
+    (*Vect04) = 0.326640741219094f * x1a + 0.135299025036549f * x1b;
+    (*Vect05) = 0.707106781186547f * (x2f + x37);
+    (*Vect06) = 0.707106781186547f * (x20 - x21);
+    (*Vect07) = 0.707106781186547f * (x2e + x35);
+    (*Vect08) = 0.25f * (x18 - x19);
+    (*Vect09) = 0.707106781186547f * (x2e - x35);
+    (*Vect10) = 0.707106781186547f * (x20 + x21);
+    (*Vect11) = 0.707106781186547f * (x30 - x36);
+    (*Vect12) = 0.135299025036549f*x1a - 0.326640741219094f*x1b;
+    (*Vect13) = 0.707106781186547f * (x30 + x36);
+    (*Vect14) = 0.25f * (x1e + x1f);
+    (*Vect15) = 0.25f * (x31 + x32);
+}
 
 template<typename T, int Step>
-__device__ void CUDAsubroutineInplaceIDCT16vector(T *Vect0) {}
+__device__ void  CUDAsubroutineInplaceIDCT16vector(T *Vect00) {
+    T *Vect01 = Vect00 + Step;
+    T *Vect02 = Vect01 + Step;
+    T *Vect03 = Vect02 + Step;
+    T *Vect04 = Vect03 + Step;
+    T *Vect05 = Vect04 + Step;
+    T *Vect06 = Vect05 + Step;
+    T *Vect07 = Vect06 + Step;
+
+    T *Vect08 = Vect00 + (Step << 3);
+    T *Vect09 = Vect08 + Step;
+    T *Vect10 = Vect09 + Step;
+    T *Vect11 = Vect10 + Step;
+    T *Vect12 = Vect11 + Step;
+    T *Vect13 = Vect12 + Step;
+    T *Vect14 = Vect13 + Step;
+    T *Vect15 = Vect14 + Step;
+
+    const float x00 =  1.4142135623731f   * (*Vect00);
+    const float x01 =  1.40740373752638f  * (*Vect01) + 0.138617169199091f * (*Vect15);
+    const float x02 =  1.38703984532215f  * (*Vect02) + 0.275899379282943f * (*Vect14);
+    const float x03 =  1.35331800117435f  * (*Vect03) + 0.410524527522357f * (*Vect13);
+    const float x04 =  1.30656296487638f  * (*Vect04) + 0.541196100146197f * (*Vect12);
+    const float x05 =  1.24722501298667f  * (*Vect05) + 0.666655658477747f * (*Vect11);
+    const float x06 =  1.17587560241936f  * (*Vect06) + 0.785694958387102f * (*Vect10);
+    const float x07 =  1.09320186700176f  * (*Vect07) + 0.897167586342636f * (*Vect09);
+    const float x08 =  1.4142135623731f   * (*Vect08);
+    const float x09 = -0.897167586342636f * (*Vect07) + 1.09320186700176f * (*Vect09);
+    const float x0a =  0.785694958387102f * (*Vect06) - 1.17587560241936f * (*Vect10);
+    const float x0b = -0.666655658477747f * (*Vect05) + 1.24722501298667f * (*Vect11);
+    const float x0c =  0.541196100146197f * (*Vect04) - 1.30656296487638f * (*Vect12);
+    const float x0d = -0.410524527522357f * (*Vect03) + 1.35331800117435f * (*Vect13);
+    const float x0e =  0.275899379282943f * (*Vect02) - 1.38703984532215f * (*Vect14);
+    const float x0f = -0.138617169199091f * (*Vect01) + 1.40740373752638f * (*Vect15);
+    const float x12 = x00 + x08;
+    const float x13 = x01 + x07;
+    const float x14 = x02 + x06;
+    const float x15 = x03 + x05;
+    const float x16 = 1.4142135623731f*x04;
+    const float x17 = x00 - x08;
+    const float x18 = x01 - x07;
+    const float x19 = x02 - x06;
+    const float x1a = x03 - x05;
+    const float x1d = x12 + x16;
+    const float x1e = x13 + x15;
+    const float x1f = 1.4142135623731f*x14;
+    const float x20 = x12 - x16;
+    const float x21 = x13 - x15;
+    const float x22 = 0.25f * (x1d - x1f);
+    const float x23 = 0.25f * (x20 + x21);
+    const float x24 = 0.25f * (x20 - x21);
+    const float x25 = 1.4142135623731f*x17;
+    const float x26 = 1.30656296487638f*x18 + 0.541196100146197f*x1a;
+    const float x27 = 1.4142135623731f*x19;
+    const float x28 = -0.541196100146197f*x18 + 1.30656296487638f*x1a;
+    const float x29 = 0.176776695296637f * (x25 + x27) + 0.25f*x26;
+    const float x2a = 0.25f * (x25 - x27);
+    const float x2b = 0.176776695296637f * (x25 + x27) - 0.25f*x26;
+    const float x2c = 0.353553390593274f*x28;
+    const float x1b = 0.707106781186547f * (x2a - x2c);
+    const float x1c = 0.707106781186547f * (x2a + x2c);
+    const float x2d = 1.4142135623731f*x0c;
+    const float x2e = x0b + x0d;
+    const float x2f = x0a + x0e;
+    const float x30 = x09 + x0f;
+    const float x31 = x09 - x0f;
+    const float x32 = x0a - x0e;
+    const float x33 = x0b - x0d;
+    const float x37 = 1.4142135623731f*x2d;
+    const float x38 = 1.30656296487638f*x2e + 0.541196100146197f*x30;
+    const float x39 = 1.4142135623731f*x2f;
+    const float x3a = -0.541196100146197f*x2e + 1.30656296487638f*x30;
+    const float x3b = 0.176776695296637f * (x37 + x39) + 0.25f*x38;
+    const float x3c = 0.25f * (x37 - x39);
+    const float x3d = 0.176776695296637f * (x37 + x39) - 0.25f*x38;
+    const float x3e = 0.353553390593274f*x3a;
+    const float x34 = 0.707106781186547f * (x3c - x3e);
+    const float x35 = 0.707106781186547f * (x3c + x3e);
+    const float x3f = 1.4142135623731f*x32;
+    const float x40 = x31 + x33;
+    const float x41 = x31 - x33;
+    const float x42 = 0.25f * (x3f + x40);
+    const float x43 = 0.25f * (x3f - x40);
+    const float x44 = 0.353553390593274f*x41;
+    (*Vect00) = 0.176776695296637f * (x1d + x1f) + 0.25f * x1e;
+    (*Vect01) = 0.707106781186547f * (x29 + x3d);
+    (*Vect02) = 0.707106781186547f * (x29 - x3d);
+    (*Vect03) = 0.707106781186547f * (x23 - x43);
+    (*Vect04) = 0.707106781186547f * (x23 + x43);
+    (*Vect05) = 0.707106781186547f * (x1b - x35);
+    (*Vect06) = 0.707106781186547f * (x1b + x35);
+    (*Vect07) = 0.707106781186547f * (x22 + x44);
+    (*Vect08) = 0.707106781186547f * (x22 - x44);
+    (*Vect09) = 0.707106781186547f * (x1c + x34);
+    (*Vect10) = 0.707106781186547f * (x1c - x34);
+    (*Vect11) = 0.707106781186547f * (x24 + x42);
+    (*Vect12) = 0.707106781186547f * (x24 - x42);
+    (*Vect13) = 0.707106781186547f * (x2b - x3b);
+    (*Vect14) = 0.707106781186547f * (x2b + x3b);
+    (*Vect15) = 0.176776695296637f * (x1d + x1f) - 0.25f*x1e;
+}
 
 template<typename T, int BLOCK_SIZE>
 __device__ void dctBlock(T shared_tmp[BLOCK_SIZE][BLOCK_SIZE + 1], int thWorker) {
@@ -570,8 +760,8 @@ RGY_ERR NVEncFilterDenoiseDct::denoise(RGYFrameInfo *pOutputFrame, const RGYFram
 #if 1
     std::swap(bufSrc, bufDst);
     static const std::map<int, decltype(denoise_frame<float, 32, 8>)*> func_list = {
-        { 8,  denoise_frame<float, 32, 8>  },
-        //{ 16, denoise_frame<uint16_t, 16, 16> },
+        { 8,  denoise_frame<float, 32,  8> },
+        { 16, denoise_frame<float, 32, 16> },
     };
     if (func_list.count(prm->dct.block_size) == 0) {
         AddMessage(RGY_LOG_ERROR, _T("unsupported block_size %d.\n"), prm->dct.block_size);
