c0309d0f981ed20874891a84e590e4cf3a516e87
rigaya <rigaya34589@live.jp>
2024-04-13 08:20:33 +0900

    Tuning Infoのログ出力の問題が改行されないのを修正。

--- NVEncCore/NVEncCore.cpp
+++ NVEncCore/NVEncCore.cpp
@@ -5100,7 +5100,7 @@ tstring NVEncCore::GetEncodingParamsInfo(int output_level) {
     if (m_dev->encoder()->checkAPIver(12, 1)) {
         add_str(RGY_LOG_INFO, _T("Split Enc Mode %s\n"), get_chr_from_value(list_split_enc_mode, m_stCreateEncodeParams.splitEncodeMode));
     }
-    add_str(RGY_LOG_INFO, _T("Tuning Info    "), get_chr_from_value(list_tuning_info, m_stCreateEncodeParams.tuningInfo));
+    add_str(RGY_LOG_INFO, _T("Tuning Info    %s\n"), get_chr_from_value(list_tuning_info, m_stCreateEncodeParams.tuningInfo));
     tstring strLookahead = _T("Lookahead      ");
     if (m_stEncConfig.rcParams.enableLookahead) {
         strLookahead += strsprintf(_T("on, %d frames"), m_stEncConfig.rcParams.lookaheadDepth);
