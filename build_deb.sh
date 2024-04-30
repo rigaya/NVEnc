9733671368b7799b2d94874242302c7a974c9eac
rigaya <rigaya34589@live.jp>
2024-04-30 11:25:26 +0900

    Ubuntu 24.04向けパッケージビルドを作成。

--- build_deb.sh
+++ build_deb.sh
@@ -24,6 +24,9 @@ if [ -e /etc/lsb-release ]; then
     elif [ "${PACKAGE_OS_CODENAME}" = "jammy" ]; then
         PACKAGE_DEPENDS="libc6(>=2.22),libstdc++6(>=6)"
         PACKAGE_DEPENDS="${PACKAGE_DEPENDS},libavcodec58,libavutil56,libavformat58,libswresample3,libavfilter7,libavdevice58,libass9"
+    elif [ "${PACKAGE_OS_CODENAME}" = "noble" ]; then
+        PACKAGE_DEPENDS="libc6(>=2.22),libstdc++6(>=6)" 
+        PACKAGE_DEPENDS="${PACKAGE_DEPENDS},libavcodec60,libavutil58,libavformat60,libswresample4,libavfilter9,libavdevice60,libass9"
     else
         echo "${PACKAGE_OS_ID}${PACKAGE_OS_VER} ${PACKAGE_OS_CODENAME} not supported in this script!"
         exit 1
