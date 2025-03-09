3ddbea388f1555db10a7004b705b2503c880b2df
rigaya <rigaya34589@live.jp>
2025-03-09 20:48:11 +0900

    nvvfx, ngx使用時はGPUメモリ使用量の問題があるため、GPUにつき1スレッドに制限。

--- NVEncCore/NVEncCore.cpp
+++ NVEncCore/NVEncCore.cpp
@@ -747,11 +747,28 @@ RGY_ERR NVEncCore::InitParallelEncode(InEncodeVideoParam *inputParam, std::vecto
     if (inputParam->ctrl.parallelEnc.isParent() && m_deviceUsage) {
         m_deviceUsage->close();
     }
+    // nvvfx, ngx使用時はGPUメモリ使用量の問題があるため、GPUにつき1スレッドに制限する
+    const bool limitOnePerGPU = inputParam->vppnv.nvvfxArtifactReduction.enable
+        || inputParam->vppnv.nvvfxDenoise.enable
+        || inputParam->vppnv.ngxTrueHDR.enable
+        || isNvvfxResizeFiter(inputParam->vpp.resize_algo)
+        || isNgxResizeFiter(inputParam->vpp.resize_algo);
     if (inputParam->ctrl.parallelEnc.parallelCount < 0) {
-        inputParam->ctrl.parallelEnc.parallelCount = std::max(std::accumulate(gpuList.begin(), gpuList.end(), 0, [&](int sum, const auto& gpu) {
-            return sum + ((gpu) ? gpu->encoder_count(m_stCodecGUID) : 0);
-        }) + m_dev->encoder_count(m_stCodecGUID), (int)gpuList.size());
+        inputParam->ctrl.parallelEnc.parallelCount = (limitOnePerGPU)
+            ? (int)gpuList.size()
+            : std::max(std::accumulate(gpuList.begin(), gpuList.end(), 0, [&](int sum, const auto& gpu) {
+                return sum + ((gpu) ? gpu->encoder_count(m_stCodecGUID) : 0);
+            }) + m_dev->encoder_count(m_stCodecGUID), (int)gpuList.size());
         PrintMes(RGY_LOG_DEBUG, _T("parallelCount set to %d\n"), inputParam->ctrl.parallelEnc.parallelCount);
+    } else if (limitOnePerGPU && inputParam->ctrl.parallelEnc.parallelCount > (int)gpuList.size()) {
+        inputParam->ctrl.parallelEnc.parallelCount = (int)gpuList.size();
+        if (inputParam->ctrl.parallelEnc.parallelCount <= 1) {
+            PrintMes(RGY_LOG_WARN, _T("Parallel encoding disabled, as nvvfx/ngx filter is enabled, which has large GPU RAM usage.\n"));
+            inputParam->ctrl.parallelEnc.parallelCount = 0;
+            inputParam->ctrl.parallelEnc.parallelId = -1;
+            return RGY_ERR_NONE;
+        }
+        PrintMes(RGY_LOG_WARN, _T("Parallel count limited to %d, as nvvfx/ngx filter is enabled, which has large GPU RAM usage.\n"), inputParam->ctrl.parallelEnc.parallelCount);
     }
     m_parallelEnc = std::make_unique<RGYParallelEnc>(m_pLog);
     if ((sts = m_parallelEnc->parallelRun(inputParam, m_pFileReader.get(), m_outputTimebase, m_pStatus.get())) != RGY_ERR_NONE) {
