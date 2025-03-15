7b2bd8a858b4c2ab6e18dfb2454bb24ca01c9f9e
rigaya <rigaya34589@live.jp>
2025-03-15 09:33:39 +0900

    Windowsではdx11、Linuxではvulkanの対応状況のみを表示するように。

--- NVEncCore/NVEncCmd.cpp
+++ NVEncCore/NVEncCmd.cpp
@@ -64,8 +64,11 @@ tstring GetNVEncVersion() {
     version += strsprintf(_T("  nvof fruc  : %s\n"), ENABLED_INFO[ENABLE_NVOFFRUC]);
     version += strsprintf(_T("  libass     : %s\n"), ENABLED_INFO[ENABLE_LIBASS_SUBBURN]);
     version += strsprintf(_T("  libdovi    : %s\n"), ENABLED_INFO[ENABLE_LIBDOVI]);
+#if !(defined(_WIN32) || defined(_WIN64))
     version += strsprintf(_T("  d3d11      : %s\n"), ENABLED_INFO[ENABLE_D3D11]);
+#else
     version += strsprintf(_T("  vulkan     : %s\n"), ENABLED_INFO[ENABLE_VULKAN]);
+#endif
     version += strsprintf(_T("  libplacebo : %s\n"), ENABLED_INFO[ENABLE_LIBPLACEBO]);
     version += strsprintf(_T("  vmaf       : %s\n"), ENABLED_INFO[ENABLE_VMAF]);
     return version;
