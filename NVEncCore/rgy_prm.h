a642717b0485c1547fce725064a1c364bb1c40f5
rigaya <rigaya34589@live.jp>
2025-03-31 18:22:04 +0900

    VCEEncの更新を反映。

--- NVEncCore/rgy_prm.h
+++ NVEncCore/rgy_prm.h
@@ -80,7 +80,7 @@ static const int RGY_AUDIO_QUALITY_DEFAULT = 0;
 #define ENABLE_VPP_FILTER_DELOGO_MULTIADD  (             ENCODER_NVENC)
 #define ENABLE_VPP_ORDER                   (CLFILTERS_AUF)
 
-#define ENABLE_PARALLEL_ENC            (ENCODER_QSV   || ENCODER_NVENC || ENCODER_VCEENC)
+#define ENABLE_PARALLEL_ENC            (ENCODER_QSV   || ENCODER_NVENC || (ENCODER_VCEENC && ENABLE_D3D11))
 
 enum class VppType : int {
     VPP_NONE,
