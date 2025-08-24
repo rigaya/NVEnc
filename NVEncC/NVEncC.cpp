593f9efb094b1f23f128b20200f9cbdd82895c18
rigaya <rigaya34589@live.jp>
2025-08-24 14:58:48 +0900

    --check-codecsなどで映像コーデックの表示に対応。

--- NVEncC/NVEncC.cpp
+++ NVEncC/NVEncC.cpp
@@ -186,7 +186,7 @@ int parse_print_options(const TCHAR *option_name, const TCHAR *arg1, const RGYPa
     }
     if (0 == _tcscmp(option_name, _T("check-codecs"))) {
         _ftprintf(stdout, _T("Video\n"));
-        _ftprintf(stdout, _T("%s\n"), getAVCodecs((RGYAVCodecType)(RGY_AVCODEC_DEC), { AVMEDIA_TYPE_VIDEO }).c_str());
+        _ftprintf(stdout, _T("%s\n"), getAVCodecs((RGYAVCodecType)(RGY_AVCODEC_DEC | RGY_AVCODEC_ENC), { AVMEDIA_TYPE_VIDEO }).c_str());
         _ftprintf(stdout, _T("\nAudio\n"));
         _ftprintf(stdout, _T("%s\n"), getAVCodecs((RGYAVCodecType)(RGY_AVCODEC_DEC | RGY_AVCODEC_ENC), { AVMEDIA_TYPE_AUDIO }).c_str());
         _ftprintf(stdout, _T("\nSubtitles\n"));
@@ -196,6 +196,8 @@ int parse_print_options(const TCHAR *option_name, const TCHAR *arg1, const RGYPa
         return 1;
     }
     if (0 == _tcscmp(option_name, _T("check-encoders"))) {
+        _ftprintf(stdout, _T("Video\n"));
+        _ftprintf(stdout, _T("%s\n"), getAVCodecs((RGYAVCodecType)(RGY_AVCODEC_ENC), { AVMEDIA_TYPE_VIDEO }).c_str());
         _ftprintf(stdout, _T("Audio\n"));
         _ftprintf(stdout, _T("%s\n"), getAVCodecs((RGYAVCodecType)(RGY_AVCODEC_ENC), { AVMEDIA_TYPE_AUDIO }).c_str());
         _ftprintf(stdout, _T("\nSubtitles\n"));
