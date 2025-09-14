d37d43761197361835b2d4cc7f4ceed409c65113
rigaya <rigaya34589@live.jp>
2025-09-14 12:05:04 +0900

    NVEncNVSDKNGXのビルドを更新。

--- NVEncCore/rgy_version.h
+++ NVEncCore/rgy_version.h
@@ -29,9 +29,9 @@
 #ifndef __RGY_CONFIG_H__
 #define __RGY_CONFIG_H__
 
-#define VER_FILEVERSION             0,9,1,2
-#define VER_STR_FILEVERSION          "9.01"
-#define VER_STR_FILEVERSION_TCHAR _T("9.01")
+#define VER_FILEVERSION             0,9,2,0
+#define VER_STR_FILEVERSION          "9.02"
+#define VER_STR_FILEVERSION_TCHAR _T("9.02")
 
 #ifdef _M_IX86
 #define BUILD_ARCH_STR _T("x86")
