5f6f647d092ecd01f97fae7881ef3db99d8d47ce
rigaya <rigaya34589@live.jp>
2026-05-28 21:03:22 +0900

    vpp-degrain CUDA実装を分割してテンプレート化

---
+++ NVEncCore/NVEncFilterDegrainU8Blk8.cu
@@ -0,0 +1,27 @@
+﻿#include "NVEncFilterDegrain.cuh"
+
+RGY_ERR launchNVEncDegrainMotionSearchSearchParallelU8Blk8(
+    const uint8_t *sourcePlane, const uint8_t *referencePlane, CUMemBuf &vectors,
+    const int pitch, const int width, const int height, const int planeBase, const int blockCount,
+    const RGYDegrainBlockLayout &layout, const int pel, const int subpelInterp,
+    const int pad, const int motionCostScale, const int lowSadWeightScale,
+    const int zeroCandidateCostScale, const int frameAverageCandidateCostScale,
+    const int newCandidateCostScale, const int level, cudaStream_t stream) {
+    return launchNVEncDegrainMotionSearchSearchParallelBlock<uint8_t, 8>(
+        sourcePlane, referencePlane, vectors, pitch, width, height, planeBase, blockCount, layout,
+        pel, subpelInterp, pad, motionCostScale, lowSadWeightScale, zeroCandidateCostScale,
+        frameAverageCandidateCostScale, newCandidateCostScale, level, stream);
+}
+
+RGY_ERR launchNVEncDegrainMotionSearchSpatialRefineU8Blk8(
+    const uint8_t *sourcePlane, const uint8_t *referencePlane,
+    CUMemBuf &vectors, const CUMemBuf &vectorsPrev, CUMemBuf &vectorsFinal,
+    const int pitch, const int width, const int height, const int planeBase, const int finalBase,
+    const int blockCount, const RGYDegrainBlockLayout &layout,
+    const int pel, const int subpelInterp, const int pad, const int motionCostScale,
+    const int lowSadWeightScale, const int newCandidateCostScale, cudaStream_t stream) {
+    return launchNVEncDegrainMotionSearchSpatialRefineBlock<uint8_t, 8>(
+        sourcePlane, referencePlane, vectors, vectorsPrev, vectorsFinal, pitch, width, height,
+        planeBase, finalBase, blockCount, layout, pel, subpelInterp, pad, motionCostScale,
+        lowSadWeightScale, newCandidateCostScale, stream);
+}
