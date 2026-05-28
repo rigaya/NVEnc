a5c1978a0021c93c2a2fd140dfc9b3112c4a98fb
rigaya <rigaya34589@live.jp>
2026-05-28 22:30:30 +0900

    --vpp-nnediのCUDA predictorを分割コンパイル化

---
+++ NVEncCore/NVEncFilterNnediU16Fast.cu
@@ -0,0 +1,10 @@
+﻿#include "NVEncFilterNnedi.cuh"
+
+RGY_ERR launchNVEncNnediPredictorU16Fast(RGYFrameInfo& dstPlane, RGYFrameInfo& refPlane,
+    const CUMemBuf *predictorWeightBuf, CUMemBuf *workNNBuf, CUMemBuf *numBlocksBuf,
+    int dstOffset, int refEvalOffset, int width4, int height, int valMin, int valMax,
+    int nsize, int nns, cudaStream_t stream) {
+    return launchPredictorNsize<uint16_t, 16, VPP_NNEDI_QUALITY_FAST>(
+        dstPlane, refPlane, predictorWeightBuf, workNNBuf, numBlocksBuf,
+        dstOffset, refEvalOffset, width4, height, valMin, valMax, nsize, nns, stream);
+}
---
+++ NVEncCore/NVEncFilterNnediU16Slow.cu
@@ -0,0 +1,10 @@
+﻿#include "NVEncFilterNnedi.cuh"
+
+RGY_ERR launchNVEncNnediPredictorU16Slow(RGYFrameInfo& dstPlane, RGYFrameInfo& refPlane,
+    const CUMemBuf *predictorWeightBuf, CUMemBuf *workNNBuf, CUMemBuf *numBlocksBuf,
+    int dstOffset, int refEvalOffset, int width4, int height, int valMin, int valMax,
+    int nsize, int nns, cudaStream_t stream) {
+    return launchPredictorNsize<uint16_t, 16, VPP_NNEDI_QUALITY_SLOW>(
+        dstPlane, refPlane, predictorWeightBuf, workNNBuf, numBlocksBuf,
+        dstOffset, refEvalOffset, width4, height, valMin, valMax, nsize, nns, stream);
+}
