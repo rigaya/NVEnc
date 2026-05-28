0e21facfbdbcb7d2bfd30713cf5ebd46622239a2
rigaya <rigaya34589@live.jp>
2026-05-29 08:40:11 +0900

    --vpp-rtgmcのCUDA SearchPrefilterを分割コンパイル化

---
+++ NVEncCore/NVEncFilterRtgmcSearchPrefilterU16TRP.cu
@@ -0,0 +1,17 @@
+﻿// -----------------------------------------------------------------------------------------
+// NVEnc by rigaya
+// -----------------------------------------------------------------------------------------
+
+#include "NVEncFilterRtgmcSearchPrefilter.cuh"
+
+const NVEncRtgmcSearchPrefilterLaunchFuncs *getNVEncRtgmcSearchPrefilterU16TRP() {
+    return getNVEncRtgmcSearchPrefilterLaunchFuncs<uint16_t, -1>();
+}
+
+RGY_ERR launchRtgmcSearchPrefilterDebugU16(
+    const int debugStage, const RGYFrameInfo &prev2, const RGYFrameInfo &prev, const RGYFrameInfo &cur,
+    const RGYFrameInfo &next, const RGYFrameInfo &next2, const RGYFrameInfo &dst,
+    const uint32_t repairProfile, const int smoothRadius, cudaStream_t stream) {
+    return launchRtgmcSearchPrefilterDebugTyped<uint16_t>(
+        debugStage, prev2, prev, cur, next, next2, dst, repairProfile, smoothRadius, stream);
+}
---
+++ NVEncCore/NVEncFilterRtgmcSearchPrefilterU8TR0.cu
@@ -0,0 +1,9 @@
+﻿// -----------------------------------------------------------------------------------------
+// NVEnc by rigaya
+// -----------------------------------------------------------------------------------------
+
+#include "NVEncFilterRtgmcSearchPrefilter.cuh"
+
+const NVEncRtgmcSearchPrefilterLaunchFuncs *getNVEncRtgmcSearchPrefilterU8TR0() {
+    return getNVEncRtgmcSearchPrefilterLaunchFuncs<uint8_t, 0>();
+}
